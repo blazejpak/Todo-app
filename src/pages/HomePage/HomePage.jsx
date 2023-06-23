41377de2502618912a53f3ec5ae13fd842d14119
blazejpak <blazej.pak@gmail.com>
2023-06-23 21:50:17 +0200

    solvedImageProblem

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
   }, [isDarkTheme]);
 
   const imageBg =
-    "before:absolute before:top-0 before:left-0 before:h-[200px] before:w-full  dark:before:bg-gradient-to-r dark:before:from-sky-900/70 dark:before:to-fuchsia-900/70  before:bg-gradient-to-r before:from-sky-500/70 before:to-fuchsia-500/70";
+    "before:absolute before:top-0 before:left-0 before:h-[200px] before:w-full  dark:before:bg-gradient-to-r dark:before:from-sky-900 dark:before:to-fuchsia-900  before:bg-gradient-to-r before:from-sky-500/70 before:to-fuchsia-500/70";
 
   return (
     <>
