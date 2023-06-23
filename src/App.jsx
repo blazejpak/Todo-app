d78d42e3a387997c41d246e4549ae173047c7d48
blazejpak <blazej.pak@gmail.com>
2023-06-23 22:01:18 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage/HomePage.jsx";
 
 function App() {
   const imageBg =
-    "before:absolute before:top-0 before:left-0 before:h-[300px] before:w-full  dark:before:bg-gradient-to-r dark:before:from-sky-900 dark:before:to-fuchsia-900  before:bg-gradient-to-r before:from-sky-500/70 before:to-fuchsia-500/70";
+    "before:absolute before:top-0 before:left-0 before:h-[300px] before:w-full  dark:before:bg-gradient-to-r dark:before:from-sky-800 dark:before:to-fuchsia-800  before:bg-gradient-to-r before:from-sky-500/70 before:to-fuchsia-500/70";
 
   return (
     <ContentProvider>
